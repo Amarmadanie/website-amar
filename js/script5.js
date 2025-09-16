def1c7e6430f8d1afbe7c508a0f67b9ba0fe5ed3
Amarmadanie <128025448+Amarmadanie@users.noreply.github.com>
2025-09-16 10:35:45 +0700

    Update script5.js

--- js/script5.js
+++ js/script5.js
@@ -62,3 +62,14 @@
       }
       lastScroll = currentScroll;
     });
+
+// contoh: ketika tombol dengan id="btnCheckout" diklik
+document.getElementById("btnCheckout").addEventListener("click", function() {
+  if (typeof gtag !== "undefined") {
+    gtag('event', 'conversion', {
+      'send_to': 'AW-17552436630/PHs-CJOSk5kbEJbb07FB'
+    });
+    console.log("Google Ads Conversion terkirim!");
+  }
+});
+
