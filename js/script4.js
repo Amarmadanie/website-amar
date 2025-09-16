03b1b0d6eaeacd9d87cc4b2a804212aaf139b74f
Amarmadanie <128025448+Amarmadanie@users.noreply.github.com>
2025-09-16 10:35:26 +0700

    Update script4.js

--- js/script4.js
+++ js/script4.js
@@ -63,3 +63,12 @@
 
     // Tombol hitung
     document.getElementById('btnHitung').addEventListener('click', hitungKredit);
+// contoh: ketika tombol dengan id="btnCheckout" diklik
+document.getElementById("btnCheckout").addEventListener("click", function() {
+  if (typeof gtag !== "undefined") {
+    gtag('event', 'conversion', {
+      'send_to': 'AW-17552436630/PHs-CJOSk5kbEJbb07FB'
+    });
+    console.log("Google Ads Conversion terkirim!");
+  }
+});
